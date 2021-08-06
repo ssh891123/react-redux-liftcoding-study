0777a671c8ac9b913575f4edfb4e0ccb3320ff77
sunghyun_son <sunghyun_son@tmax.co.kr>
2021-08-06 09:31:50 +0900

    [feature] 생활코딩 react redux강의 clone 코딩
    redux가 있을때와 없을때 state을 변경하는 방법 실행
    redux가 없을때는 depth에 따라 값을 전달하는게 복잡함
    redux가 있을때는 값을 저장해두고 직접 전달할 수 있어서 좋음(Manager OR SingleTon방식이랑 비슷함)

---
+++ src/components/DisplayNumber.jsx
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import store from "../store";
+
+export default class DisplayNumber extends Component {
+  state = {number:store.getState().number}
+  constructor(props) {
+    super(props);
+    store.subscribe(function() {
+      this.setState({number:store.getState().number});
+    }.bind(this));
+  }
+  render() {
+    return (
+      <div>
+        <h1>Display Number</h1>
+        <p>
+        No-Redux
+        <input type="type" value={this.props.number} readOnly></input>
+        </p>
+        <p>
+        Redux
+        <input type="type" value={this.state.number} readOnly></input>
+        </p>
+      </div>
+    )
+  }
+}
